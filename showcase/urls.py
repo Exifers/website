e131f41f3ff7ef195b38c4fac74debc75ce3449b
Emmanuel <meric.emmanuel@gmail.com>
2019-02-08 11:50:37 +0100

    [Frontend][Navbar]: added links

--- showcase/urls.py
+++ showcase/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from showcase import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='showcase'),
+    path('about', views.index, name='about'),
+    path('download', views.index, name='download'),
+    path('documentation', views.index, name='documentation'),
+    path('leaderboard', views.index, name='leaderboard'),
+    path('credits', views.index, name='credits')
 ]
