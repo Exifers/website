74ce10c7cafb3201570c04fde499047e24f466e5
Emmanuel <meric.emmanuel@gmail.com>
2019-05-09 18:51:03 +0200

    added dj database urls

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from google.oauth2 import service_account
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
