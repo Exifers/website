353605bc935f29f7d75ea39ce08ee9194556453e
Emmanuel <meric.emmanuel@gmail.com>
2019-07-14 18:04:34 +0200

    [Deploy]: updated db settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
 
 if PRODUCTION:
     DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
+        'default': dj_database_url.config()
     }
 
 # [END db_setup]
